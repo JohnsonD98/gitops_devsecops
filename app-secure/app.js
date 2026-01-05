ff4487c93d65cd490b8b1e86e19339b35684a661
Sarah Lapuente <sassou.lapuente@gmail.com>
2026-01-05 18:44:07 +0100

    TP4: change node-app-secure message + bump image tag v2

--- app-secure/app.js
+++ app-secure/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Node app secure is running!');
+  res.send('Hello from Secure App');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+app.listen(3000, () => {
+  console.log('Secure app running on port 3000');
 });
+
